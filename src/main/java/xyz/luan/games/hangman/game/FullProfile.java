0ffd6f3bc010ae72bcaf4c73b3990c408cb7f649
Luan Nico <luanpotter28@hotmail.com>
2015-01-06 21:32:42 -0200

    FullProfile draft

---
+++ src/main/java/xyz/luan/games/hangman/game/FullProfile.java
@@ -0,0 +1,13 @@
+package xyz.luan.games.hangman.game;
+
+import java.util.List;
+
+import xyz.luan.games.hangman.spells.Spell;
+
+public class FullProfile {
+
+	private Profile profile;
+	private List<Spell> selectedSpells;
+	private ProfileStatuses statuses;
+	private List<FullProfile> friends;
+}
